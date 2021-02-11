6f5d6876531ee1268d55a5634b4a989b32913597
lycHub <1120805364@qq.com>
2021-02-11 15:28:44 +0800

    loading

--- src/actions/create.ts
+++ src/actions/create.ts
@@ -1,3 +1,20 @@
-export default function () {
+import ora from "ora";
+import chalk from "chalk";
 
+interface CreateOptions {
+  tool: 'webpack' | 'gulp',
+  install: boolean;
+  pkgTool: 'npm' | 'yarn';
+}
+
+export default function (projectName: string, options: CreateOptions) {
+  console.log('projectName', projectName);
+  console.log('options', options);
+  const spinner = ora(chalk.blue('初始化模版...')).start();
+  setTimeout(() => {
+    spinner.text = '正在下载...';
+  }, 2000);
+  setTimeout(() => {
+    spinner.succeed(chalk.green('模版初始化成功.'));
+  }, 5000);
 }
