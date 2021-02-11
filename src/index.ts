595a18e747ec052be6596589392641571de45cbf
lycHub <1120805364@qq.com>
2021-02-11 12:26:38 +0800

    create

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { Command } from 'commander';
+import create from "./actions/create";
 
 const program = new Command('vmc');
 program
+  .usage('create <projectName> [options]')
   .command('create <projectName>')
   .description('创建项目')
-  .option('-ao --arg-one [oneValue]', '参数一')
-  .option('-at --arg-two [towValue]', '参数二', 'b')
-  .option('-s --small', '是否是最小的', true)
-  .option('-def --defArg [type]', '默认参数', '默认的type')
-  .option('-l --long [value...]', '长参数')
-  .action((projectName, options) => {
-    console.log('projectName', projectName);
-    console.log('options', options);
-  });
+  .option('-t --tool [value]', '选择构建工具：webpack|gulp', 'webpack')
+  .option('-i --install', '是否自动安装依赖', false)
+  .option('-pt --pkg-tool [value]', 'npm or yarn?')
+  .action(create);
 
-program.parse(process.argv);
+program.addHelpText('after', `
+  Example create a project:
+    $ vmc create helloWorld
+`);
+
+program.version(require('../package.json').version).parse(process.argv);
